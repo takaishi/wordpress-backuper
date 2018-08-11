d8ac9423cf5a7599b72841b1c8414db09314b318
r_takaishi <ryo.takaishi.0@gmail.com>
2018-08-11 12:04:39 +0900

    support rotate backups

--- main.go
+++ main.go
@@ -55,6 +55,12 @@ func main() {
 		log.Fatalf("Failed to upload to s3: %s\n", err)
 	}
 
+	log.Printf("Start rotate backups\n")
+	err = RotateBackup()
+	if err != nil {
+		log.Fatalf("Failed to rotate backups: %s\n", err)
+	}
+
 	log.Printf("Finish backup to s3://%s/%s\n", bucket, dumpSubdir)
 }
 
@@ -138,5 +144,72 @@ func UploadToS3(cli *s3.S3, path string, bucket string, key string) error {
 		return errors.Wrap(err, "Failed to PutObject")
 	}
 
+	return nil
+}
+
+func RotateBackup() error {
+	backup_size := 3
+	accessKeyID := os.Getenv("AWS_ACCESS_KEY_ID")
+	secretAccessKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
+	region := os.Getenv("AWS_REGION")
+	bucket := os.Getenv("AWS_BUCKET")
+
+	sess, err := session.NewSession(&aws.Config{
+		Credentials: credentials.NewStaticCredentials(accessKeyID, secretAccessKey, ""),
+		Region:      aws.String(region),
+	})
+	if err != nil {
+		return errors.Wrap(err, "Failed to create session")
+	}
+
+	cli := s3.New(sess)
+
+	keys, err := GetDeletePrefixes(cli, bucket, backup_size)
+	if err != nil {
+		return errors.Wrap(err, "Failed to get delete prefixes")
+	}
+
+	for _, k := range keys {
+		result, err := cli.ListObjectsV2(&s3.ListObjectsV2Input{
+			Bucket:    aws.String(bucket),
+			Prefix:    aws.String(fmt.Sprintf("%s", k)),
+			Delimiter: aws.String("/"),
+		})
+		if err != nil {
+			return errors.Wrap(err, "Failed to list object")
+		}
+		for _, o := range result.Contents {
+			DeleteObject(cli, bucket, *o.Key)
+		}
+	}
+	return nil
+}
+
+func GetDeletePrefixes(cli *s3.S3, bucket string, backup_size int) ([]string, error) {
+	var keys []string
+	result, err := cli.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket:    aws.String(bucket),
+		Delimiter: aws.String("/"),
+	})
+
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to list object")
+	}
+
+	for _, k := range result.CommonPrefixes[0 : len(result.CommonPrefixes)-backup_size] {
+		keys = append(keys, *k.Prefix)
+	}
+
+	return keys, nil
+}
+
+func DeleteObject(cli *s3.S3, bucket string, key string) error {
+	_, err := cli.DeleteObject(&s3.DeleteObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		errors.Wrap(err, "Failed to delete object")
+	}
 	return nil
 }
