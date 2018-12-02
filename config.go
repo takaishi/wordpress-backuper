d38b6d51184346a3bb2af6691f77cf5e2fb0ecfb
r_takaishi <ryo.takaishi.0@gmail.com>
2018-12-02 11:03:11 +0900

    refactor: move config struct other file

---
+++ config.go
@@ -0,0 +1,26 @@
+package main
+
+type Config struct {
+	DB        DBConfig        `toml:"DB"`
+	Wordpress WordpressConfig `toml:"Wordpress"`
+	AWS       AWSConfig       `toml:"AWS"`
+}
+
+type DBConfig struct {
+	Username string `toml:"username"`
+	Password string `toml:"password"`
+	Hostname string `toml:"hostname"`
+	Port     int    `toml:"port"`
+	Database string `toml:"database"`
+}
+
+type WordpressConfig struct {
+	RootDir string `toml:"root_dir"`
+}
+
+type AWSConfig struct {
+	AccessKeyID     string `toml:"access_key_id"`
+	SecretAccessKey string `toml:"secret_access_key"`
+	Region          string `toml:"region"`
+	Bucket          string `toml:"bucket"`
+}
